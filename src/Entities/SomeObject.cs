270a0b5fb669d7c18f9f74cae190d95cf821fd11
tishchenko <rtishchenko@corepartners.com>
2025-03-13 00:45:03 +0300

    Add SomeObject entity

---
+++ src/Entities/SomeObject.cs
@@ -0,0 +1,8 @@
+namespace FinstarTestTask.Entities;
+
+public class SomeObject
+{
+    public int Number { get; set; }
+    public int Code { get; set; }
+    public string Value { get; set; } = string.Empty;
+}
